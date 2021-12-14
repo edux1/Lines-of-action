8ae3b06d1d70b15668142041d46a59457f77caba
Edu Pinto <edu.pm97@gmail.com>
2021-12-14 12:20:45 +0100

    heuristica fina

--- src/edu/upc/epsevg/prop/loa/players/tematé_otravé.java
+++ src/edu/upc/epsevg/prop/loa/players/tematé_otravé.java
@@ -21,6 +21,7 @@ public class tematé_otravé implements IPlayer {
 
     private String name;
     private ElMeuStatus s;
+    private boolean visited[][];
     
     public tematé_otravé(String name) {
         this.name = name;
@@ -46,30 +47,54 @@ public class tematé_otravé implements IPlayer {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    public int heuristica_1() {
+    public int heuristica_1(CellType jugador) {
         int score = 0;
-        boolean visited[][] = new boolean[s.getSize()][s.getSize()];
+        int grup1_max = 0;
+        int grup2_max = 0;
+        visited = new boolean[s.getSize()][s.getSize()];
         Arrays.fill(visited, false);
         
         for (int i = 0; i < s.getSize(); i++) {
             for(int j= 0; j < s.getSize(); j++) {
                 
                 if(s.getPos(i, j) != CellType.EMPTY && !visited[i][j]) {
-                   // if()
-                     //   busca_veines(s.getPos(i,j), s.getCurrentPlayer());
+                    //Creo que se puede optimizar la segunda condición del if
+                    if(s.getPos(i, j) == jugador && grup1_max < s.getNumberOfPiecesPerColor(jugador)/2)
+                        grup1_max = Math.max(busca_veines(i, j, s.getPos(i,j)), grup1_max);
+                    //Creo que se puede optimizar la segunda condición del if
+                    else if(grup2_max < s.getNumberOfPiecesPerColor(CellType.opposite(jugador))/2)
+                        grup2_max = Math.max(busca_veines(i, j, s.getPos(i,j)), grup2_max);
                 }
-                visited[i][j] = true;   
-                
+                if(grup1_max >= s.getNumberOfPiecesPerColor(jugador)/2 && grup2_max >= s.getNumberOfPiecesPerColor(CellType.opposite(jugador))/2)
+                    break;
+                visited[i][j] = true;  //puede irse 
             }
+            if(grup1_max >= s.getNumberOfPiecesPerColor(jugador)/2 && grup2_max >= s.getNumberOfPiecesPerColor(CellType.opposite(jugador))/2)
+                break;
         }
         
         // Retornem el valor de l'heuristica
-        return score;
+        return grup1_max - grup2_max;
     }
     
-    //To Do: Uri
-    public int busca_veines() {
-        return 0;
+    
+    public int busca_veines(int fil, int col, CellType jugador) {
+        if (visited[fil][col]) return 0;
+        int valor = 1;
+        visited[fil][col] = true;
+        
+        for (int i = fil-1; i < fil+1 && i <= visited.length; i++) {
+            if(i < 0) i = 0;
+            for (int j = col-1; j < col+1 && j <= visited.length ; j++) {
+                if(j < 0) j = 0;
+                if (i != fil && j != col && s.getPos(i, j) == jugador) {
+                    valor += busca_veines(i, j, jugador);
+                }
+            }
+        }
+        
+        return valor;
+        
     }
     
 }
