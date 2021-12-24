0f26331243c28c20b96af5ebf382bc515175b400
orifer <oriol.fernandez@hotmail.com>
2021-12-24 20:53:01 +0100

    JavaDoc: Transposition.java

--- src/edu/upc/epsevg/prop/loa/Transposition.java
+++ src/edu/upc/epsevg/prop/loa/Transposition.java
@@ -3,11 +3,20 @@ package edu.upc.epsevg.prop.loa;
 import java.awt.*;
 import java.util.Map;
 
+/**
+ * Classe que guarda informació útil per a l'implementació del minmax Zobrist
+ */
 public class Transposition {
     private Map.Entry<Point, Point> millorMoviment;
     private int profunditat;
     private int heuristica;
 
+    /**
+     * Constructor principal
+     * @param millorMoviment El millor moviment amb informacio de la fitxa origen i desti
+     * @param profunditat La profunditat de l'estat
+     * @param heuristica L'heuristica de l'estat
+     */
     public Transposition(Map.Entry<Point, Point> millorMoviment, int profunditat, int heuristica) {
         this.millorMoviment = millorMoviment;
         this.profunditat = profunditat;
