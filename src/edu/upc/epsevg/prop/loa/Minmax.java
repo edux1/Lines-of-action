4805079e107ec692ce8ae0c98d40ac5c667ff4d2
orifer <oriol.fernandez@hotmail.com>
2021-12-24 10:54:08 +0100

    Minmax corregido con los cambios de getCurrentPlayer()

--- src/edu/upc/epsevg/prop/loa/Minmax.java
+++ src/edu/upc/epsevg/prop/loa/Minmax.java
@@ -10,21 +10,24 @@ public class Minmax {
     public static Entry<Point, Point> Tria_Moviment(ElMeuStatus estat, int profunditat) {
         int valor = Integer.MIN_VALUE;
         Entry<Point, Point> millorMoviment = Map.entry(new Point(),new Point());
-        
-        for (int i = 0; i < estat.getNumberOfPiecesPerColor(estat.getCurrentPlayer()); i++) {
-            Point posAct = estat.getPiece(estat.getCurrentPlayer(), i);
+
+        CellType jugador = estat.getCurrentPlayer();
+
+        for (int i = 0; i < estat.getNumberOfPiecesPerColor(jugador); i++) {
+            Point posAct = estat.getPiece(jugador, i);
             for (Point pos : estat.getMoves(posAct)) {
                 ElMeuStatus aux = new ElMeuStatus(estat);
                 aux.movePiece(posAct, pos);
                 
                 //Caso ganador
-                if(aux.isGameOver() && aux.GetWinner() == estat.getCurrentPlayer())
+                if(aux.isGameOver() && aux.GetWinner() == jugador)
                     return Map.entry(posAct, pos);
-                                
-                int min = minvalor(aux, profunditat-1);
-                if (min >= valor) {
-                    valor = min;
-                    millorMoviment = Map.entry(posAct, pos);
+                else if(!aux.isGameOver()) {
+                    int min = minvalor(aux, profunditat-1, jugador);
+                    if (min >= valor) {
+                        valor = min;
+                        millorMoviment = Map.entry(posAct, pos);
+                    }
                 }
             }
         }
@@ -33,10 +36,10 @@ public class Minmax {
     
 
     
-    public static int maxvalor(ElMeuStatus estat, int profunditat) {
+    public static int maxvalor(ElMeuStatus estat, int profunditat, CellType jugador) {
         // No podemos seguir o llegado a la hoja
         if (estat.checkGameOver() || profunditat == 0) {
-            return Heuristica.calcula(estat, CellType.opposite(estat.getCurrentPlayer())) ;
+            return Heuristica.calcula(estat, CellType.opposite(jugador)) ;
         }
 
         int valor = Integer.MIN_VALUE;
@@ -46,22 +49,22 @@ public class Minmax {
             for (Point pos : estat.getMoves(posAct)) {
                 ElMeuStatus aux = new ElMeuStatus(estat);
                 aux.movePiece(posAct, pos);
-                
+
                 // Caso ganador
-                if(aux.isGameOver() && aux.GetWinner() == estat.getCurrentPlayer())
+                if(aux.isGameOver() && aux.GetWinner() == jugador)
                     return Integer.MAX_VALUE;
-                
-                valor = Math.max(valor, minvalor(aux, profunditat - 1));
+                else if (!aux.isGameOver())
+                    valor = Math.max(valor, minvalor(aux, profunditat - 1, jugador));
             }
         }
         return valor;
     }
 
 
-    public static int minvalor(ElMeuStatus estat, int profunditat) {
+    public static int minvalor(ElMeuStatus estat, int profunditat, CellType jugador) {
         // No podemos seguir o llegado a la hoja
         if (estat.checkGameOver() || profunditat == 0) {
-            return Heuristica.calcula(estat, CellType.opposite(estat.getCurrentPlayer()));
+            return Heuristica.calcula(estat, CellType.opposite(jugador));
         }
 
         int valor = Integer.MAX_VALUE;
@@ -73,10 +76,10 @@ public class Minmax {
                 aux.movePiece(posAct, pos);
 
                 // Caso ganador
-                if(aux.isGameOver() && aux.GetWinner() == estat.getCurrentPlayer())
+                if(aux.isGameOver() && aux.GetWinner() == CellType.opposite(jugador))
                     return Integer.MIN_VALUE;
-                    
-                valor = Math.min(valor, maxvalor(aux, profunditat - 1));
+                else if (!aux.isGameOver())
+                    valor = Math.min(valor, maxvalor(aux, profunditat - 1, jugador));
             }
         }
         return valor;
