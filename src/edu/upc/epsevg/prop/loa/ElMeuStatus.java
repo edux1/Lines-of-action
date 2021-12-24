977364ddcb92adf672b326207995a0ff71f57d41
edux1 <edu.pm97@gmail.com>
2021-12-24 22:34:10 +0100

    Documentación acabada :)

--- src/edu/upc/epsevg/prop/loa/ElMeuStatus.java
+++ src/edu/upc/epsevg/prop/loa/ElMeuStatus.java
@@ -5,8 +5,8 @@ import java.util.Hashtable;
 import java.util.Map;
 
 /**
- *
- * @author Who
+ * Benvolguts al Nostre Status! :D
+ * @author Eduardo Pinto && Oriol Fernández
  */
 public class ElMeuStatus extends GameStatus {
 
@@ -14,17 +14,27 @@ public class ElMeuStatus extends GameStatus {
     private int[][][] zobristTable = new int[8][8][2];
     private Hashtable<Long, Transposition> transpositionHashtable;
 
-    // Constructor per als tests
+    /**
+     * Constructor per als tests
+     * @param tauler Tauler on es jugarà.
+     */
     public ElMeuStatus(int [][] tauler) {
         super(tauler);
     }
 
-    // Constructor pare de GameStatus
+    /**
+     * Constructor pare de GameStatus
+     * @param gameStatus Status a copiar.
+     */
     public ElMeuStatus(GameStatus gameStatus) {
         super(gameStatus);
     }
 
-    // Constructor pare de GameStatus mes implementacio amb Zobrist Hashing
+    /**
+     * Constructor pare de GameStatus més implementació amb Zobrist Hashing
+     * @param gs Status a copiar.
+     * @param zobristTable Taula per fer la millora Zobrist.
+     */
     public ElMeuStatus(GameStatus gs, int[][][] zobristTable) {
         super(gs);
         this.zobristTable = zobristTable;
@@ -35,7 +45,10 @@ public class ElMeuStatus extends GameStatus {
         System.out.println("Este estado tiene hash: " + hash);
     }
 
-    // Constructor por copia
+    /**
+     * Constructor por copia
+     * @param estat Estat del tauler.
+     */
     public ElMeuStatus(ElMeuStatus estat) {
         super(estat);
         this.hash = estat.hash;
@@ -43,6 +56,13 @@ public class ElMeuStatus extends GameStatus {
         this.transpositionHashtable = estat.transpositionHashtable;
     }
 
+    /**
+     * Constructor de ElMeuStatus
+     * @param ints Matriu
+     * @param hash Valor del hash
+     * @param zobristTable Taula Zobrist
+     * @param transpositionHashtable Taula Hash Transposicions
+     */
     public ElMeuStatus(int[][] ints, long hash, int[][][] zobristTable, Hashtable<Long, Transposition> transpositionHashtable) {
         super(ints);
         this.hash = hash;
@@ -50,6 +70,13 @@ public class ElMeuStatus extends GameStatus {
         this.transpositionHashtable = transpositionHashtable;
     }
 
+    /**
+     * Constructor de ElMeuStatus
+     * @param gameStatus Status a copiar
+     * @param hash Valor del hash
+     * @param zobristTable Taula Zobrist
+     * @param transpositionHashtable Taula Hash Transposicions
+     */
     public ElMeuStatus(GameStatus gameStatus, long hash, int[][][] zobristTable, Hashtable<Long, Transposition> transpositionHashtable) {
         super(gameStatus);
         this.hash = hash;
@@ -57,6 +84,9 @@ public class ElMeuStatus extends GameStatus {
         this.transpositionHashtable = transpositionHashtable;
     }
 
+    /**
+     * Calcula el valor del hash.
+     */
     public void calcula_hash() {
         Point p;
         hash = 0;
@@ -71,10 +101,21 @@ public class ElMeuStatus extends GameStatus {
         }
     }
 
+    /**
+     * Guarda el la profunditat, l'heuristica i el millor moviment a la taula de transposició.
+     * @param millorMoviment Punt origen i punt destí.
+     * @param profunditat Profunditat explorada.
+     * @param heuristica Heuristica que s'utilitzará.
+     */
     public void put_transposicio(Map.Entry<Point, Point> millorMoviment, int profunditat, int heuristica) {
         this.transpositionHashtable.put(hash, new Transposition(millorMoviment, profunditat, heuristica));
     }
 
+    /**
+     * Retorna una taula de transposició.
+     * @param profunditat profunditat explorada.
+     * @return Taula de transposició.
+     */
     public Transposition get_transposicio(int profunditat) {
         Transposition t = this.transpositionHashtable.get(hash);
         if (t.getProfunditat() < profunditat)
@@ -84,6 +125,10 @@ public class ElMeuStatus extends GameStatus {
         return null;
     }
 
+    /**
+     * Retorna el valor del hash
+     * @return valor del hash
+     */
     public long getHash() {
         return hash;
     }
--- src/edu/upc/epsevg/prop/loa/Transposition.java
+++ src/edu/upc/epsevg/prop/loa/Transposition.java
@@ -23,26 +23,50 @@ public class Transposition {
         this.heuristica = heuristica;
     }
 
+    /**
+     * Retorna el millorMoviment
+     * @return Punt origen i punt destí
+     */
     public Map.Entry<Point, Point> getMillorMoviment() {
         return millorMoviment;
     }
 
+    /**
+     * Estableix un nou millor moviment.
+     * @param millorMoviment Punt origen i punt destí
+     */
     public void setMillorMoviment(Map.Entry<Point, Point> millorMoviment) {
         this.millorMoviment = millorMoviment;
     }
 
+    /**
+     * Retorna la profunditat.
+     * @return Valor de la profunditat.
+     */
     public int getProfunditat() {
         return profunditat;
     }
 
+    /**
+     * Estableix una profunditat.
+     * @param profunditat Valor de la profunditat.
+     */
     public void setProfunditat(int profunditat) {
         this.profunditat = profunditat;
     }
 
+    /**
+     * Retorna una heuristica.
+     * @return Una heuristica.
+     */
     public int getHeuristica() {
         return heuristica;
     }
 
+    /**
+     * Estableix una heuristica.
+     * @param heuristica Una heuristica.
+     */
     public void setHeuristica(int heuristica) {
         this.heuristica = heuristica;
     }
